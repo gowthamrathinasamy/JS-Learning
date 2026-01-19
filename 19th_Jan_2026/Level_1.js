07885bcccef3967f6936d109d87d6d049627b2c6
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2026-01-19 11:24:53 +0530

    Level_1

---
+++ 19th_Jan_2026/Level_1.js
@@ -0,0 +1,17 @@
+console.log("Hello, JavaScript!");
+
+var a = 10;
+console.log(a);
+
+{
+    let age =23;
+    console.log("age = "+age)
+}
+
+const country = "India";
+console.log("country : "+country);
+
+var pencil = 10;
+var eraser = 1;
+sum = pencil+eraser;
+console.log("Sum of two products = "+sum);
