074b4c97d5f50951c1406934defadb01b97986da
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2026-01-19 11:57:00 +0530

    Level_2

--- 19th_Jan_2026/Level_1.js
+++ 19th_Jan_2026/Level_1.js
@@ -1,16 +1,21 @@
+//Print “Hello, JavaScript!” in the console.
 console.log("Hello, JavaScript!");
 
+//Declare a variable name using var and print it.
 var a = 10;
 console.log(a);
 
+//Declare a variable age using let and print it.
 {
     let age =23;
     console.log("age = "+age)
 }
 
+//Declare a constant country and print it.
 const country = "India";
 console.log("country : "+country);
 
+//Print the sum of two numbers using variables.
 var pencil = 10;
 var eraser = 1;
 sum = pencil+eraser;
---
+++ 19th_Jan_2026/Level_2.js
@@ -0,0 +1,69 @@
+//Write a program to check if a number is positive or negative.
+{
+    let num = 2;
+if(num>0)
+    {
+console.log("The given number '" +num+  "' is Positive")
+    }
+else
+{
+    console.log("The given number '" +num+ "' is Negative")
+}
+}
+//Check if a number is even or odd.
+
+{
+    let num = 3;
+    if(num%2==0)
+    {
+        console.log("The given number '"+num+ "' is even")
+    }
+    else
+    {
+        console.log("The Given number '"+num+ "' is Odd")
+    }
+} 
+
+//Check if a person is eligible to vote (age ≥ 18).
+
+{
+    let age = 24;
+    if (age>=18)
+    {
+        console.log("A Person is eligible to vote")
+    }
+    else
+    {
+        console.log("A Person is not eligible to vote")
+    }
+}
+
+//Compare two numbers and print which one is bigger.
+
+{
+let a =24;
+let b = 23;
+if (a>b)
+{
+    console.log("A is Bigger than B")
+
+}
+else
+{
+    console.log("B is Bigger than A")
+}
+}
+
+//Check if a number is greater than 100 or not.
+
+{
+    let number = 2002;
+    if (number > 100)
+    {
+        console.log ("The Given number '"+number+"' is greater than 100")
+    }
+    else
+        {
+        console.log ("The Given number '"+number+"' is smaller than 100")
+    }
+}
