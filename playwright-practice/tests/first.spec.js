33e7934886f1506f31031fe002af4978db8399af
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2026-01-22 01:14:37 +0530

    playwright day1: updated config and first test

--- playwright-practice/tests/first.spec.js
+++ playwright-practice/tests/first.spec.js
@@ -1,8 +1,10 @@
-const {test} = require('@playwright/test')
+const {test, expect} = require('@playwright/test');
 
-test ( 'Open Browser', async({page})=>
+test('Open Browser', async({page})=>
 {
-await page.goto("https://www.youtube.com")
+await page.goto("https://nextgen-qa.radiusdirect.net/");
+await expect(page).toHaveTitle("Nextgen QA");
+let title=await page.title();
+console.log("Title is: "+title);
 }
-
 )
