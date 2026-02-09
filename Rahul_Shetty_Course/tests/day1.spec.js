4660b8d71feadfbb86e3b10fe9aa980964dc9914
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2026-02-10 00:11:50 +0530

    Nextgen

--- Rahul_Shetty_Course/tests/day1.spec.js
+++ Rahul_Shetty_Course/tests/day1.spec.js
@@ -1,8 +1,17 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
 console.log('Hello World');
-test('Rahul Shetty Course', async ({page}) => {
-    await page.goto('https://www.youtube.com/#/');
+test('Nextgen Login', async ({page}) => {
+    await page.goto('https://nextgen-qa.radiusdirect.net/login');
+    const loginLink = page.getByRole('link', { name: 'Login with Username'});
+    await loginLink.click();
+    await page.waitForLoadState('networkidle');
+    const userName = page.getByPlaceholder("Username or Email");
+    await userName.fill('u1183420');
+    const password = page.getByPlaceholder("Password");
+    await password.pressSequentially('Iqvia123!', {delay: 100});
+    const loginButton = page.getByRole('button', { type: 'submit' });
+    await loginButton.click();
     await page.pause();
 }   
 )
