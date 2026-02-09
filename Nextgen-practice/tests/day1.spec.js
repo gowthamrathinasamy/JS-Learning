857b2dcfeab86dc08546357e92e1f944ab4994a7
[200~gowthamrathinasamy <gowtham101910@gmail.com>
2026-02-10 00:30:10 +0530

    login Nextgen

--- Nextgen-practice/tests/day1.spec.js
+++ Nextgen-practice/tests/day1.spec.js
@@ -1,16 +1,15 @@
 const {test, expect} = require('@playwright/test');
-
-console.log('Hello World');
 test('Nextgen Login', async ({page}) => {
     await page.goto('https://nextgen-qa.radiusdirect.net/login');
-    const loginLink = page.getByRole('link', { name: 'Login with Username'});
+    const loginLink = page.locator('a', { hasText: 'Login' });
+    await expect(loginLink).toBeVisible();
     await loginLink.click();
     await page.waitForLoadState('networkidle');
     const userName = page.getByPlaceholder("Username or Email");
     await userName.fill('u1183420');
     const password = page.getByPlaceholder("Password");
     await password.pressSequentially('Iqvia123!', {delay: 100});
-    const loginButton = page.getByRole('button', { type: 'submit' });
+    const loginButton = page.locator('button[type="submit"]');
     await loginButton.click();
     await page.pause();
 }   
